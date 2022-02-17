60d29eafb42256ca90a118cbd168aed3b34df25c
unknown <rs.ajaysharon2001@gmail.com>
2022-02-17 10:09:58 +0530

    17-02-22

--- db.py
+++ db.py
@@ -1,8 +1,25 @@
-'''import sqlite3
+import sqlite3
 
-connection=sqlite3.connect("login.db")
-cursor= connection.cursor()
+db=sqlite3.connect("login.db")
+cursor= db.cursor()
 
-query="""create table if not exist students()""" 
+'''
+def execute_query(sql_query):
+    with sqlite3.connect('login.db') as db:
+        csr=db.cursor()
+        result=csr.execute(sql_query)
+        db.commit()
+    return result
 
+sql_query="""SELECT * FROM admin"""
+RESULT=execute_query(sql_query)
+print(RESULT.fetchall())
+'''
+
+'''
+query="""create table admin(username text,password text)"""
+cursor.execute(query)
+query="""INSERT INTO admin VALUES('user','user22')"""
+cursor.execute(query)
+db.commit()
 '''
